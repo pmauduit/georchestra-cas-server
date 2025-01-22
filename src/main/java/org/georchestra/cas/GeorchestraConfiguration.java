3f5895fb09ea8c9aee8ba278e4746b436df42c68
Landry Breuil <breuil@craig.fr>
2025-01-22 14:05:20 +0100

    readd legacy headers, and set the height attribute on the geor-header webcomponent

--- src/main/java/org/georchestra/cas/GeorchestraConfiguration.java
+++ src/main/java/org/georchestra/cas/GeorchestraConfiguration.java
@@ -17,14 +17,23 @@ import org.springframework.context.annotation.PropertySource;
 @Data
 public class GeorchestraConfiguration {
 
+    @Value( "${useLegacyHeader:false}" )
+    boolean useLegacyHeader = false;
+
     @Value( "${headerConfigFile:}" )
     String headerConfigFile;
+
+    @Value( "${headerUrl:/header/}" )
+    String headerUrl;
 
     @Value("${headerHeight:80}")
     String headerHeight;
 
     @Value("${headerScript:https://cdn.jsdelivr.net/gh/georchestra/header@dist/header.js}")
     String headerScript;
+
+    @Value("${logoUrl:https://www.georchestra.org/public/georchestra-logo.svg}")
+    String logoUrl;
 
     @Value("${georchestraStylesheet:}")
     String georchestraStylesheet;
